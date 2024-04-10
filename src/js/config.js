65596be436957b8ed6b536bb874dbc47eae20026
Airton José Dos Santos <139139788+Airton-Santos@users.noreply.github.com>
2024-04-10 15:00:56 -0300

    new

--- src/js/config.js
+++ src/js/config.js
@@ -4,6 +4,4 @@ let body = document.querySelector('body');
 darkMode.addEventListener('click', ()=>{
     darkMode.classList.toggle ('darkAtv');
     body.classList.toggle('darkAtv');
-    var sound = document.getElementById("sound");
-    sound.play();
 })
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,15 @@
 const item = document.querySelectorAll(`.item-menu`)
 
-const pag = document.querySelectorAll('.pag');
+const pag = document.querySelectorAll('#pag');
 
 item.forEach((item, indice) =>{
     item.addEventListener(`click`, () => {
         const itemAtv = document.querySelector('.ativo.item-menu')
-        const pagatv = document.querySelector('.ativo.pag')
+        const pagatv = document.querySelector('.ativo#pag')
         itemAtv.classList.remove('ativo')
         item.classList.add('ativo')
         pag[indice].classList.add('ativo')
         pagatv.classList.remove('ativo')
-        var sound = document.getElementById("sound");
-        sound.play();
     })
 })
 
@@ -25,42 +23,19 @@ loginButton.addEventListener("click", function(e) {
     loginDiv.style.zIndex = "1000";
     document.body.appendChild(loginDiv);
     loginDiv.style.display = "block";
-    var sound = document.getElementById("sound");
-    sound.play();
 });
+<<<<<<< HEAD
+=======
 
-/*var restri = "250702"
-
-function validar() {
-    if (document.getElementById("senha").value == restri) {
-        const cadastroButton = document.getElementById("cadastro");
-        const cadastroDiv = document.getElementById("cadastroDiv");
-
-        cadastroButton.addEventListener("click", function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            loginDiv.style.position = "absolute";
-            loginDiv.style.zIndex = "1000";
-            document.body.appendChild(cadastroDiv);
-            loginDiv.style.display = "block";
-            var sound = document.getElementById("sound");
-            sound.play();
-        });
-    }else {
-        alert("Senha incorreta!")
-    }
-}*/
-
-const cadastroButton = document.getElementById("cadastro");
-const cadastroDiv = document.getElementById("cadastroDiv");
-
-cadastroButton.addEventListener("click", function(event) {
+const pass1 = document.getElementById(`pass1`).value;
+const chk = document.getElementById(`chk`)
+chk.addEventListener("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    cadastroDiv.style.position = "absolute";
-    cadastroDiv.style.zIndex = "1000";
-    document.body.appendChild(cadastroDiv);
-    cadastroDiv.style.display = "block";
-    var sound = document.getElementById("sound");
-    sound.play();
+    if (chk.checked) {
+        pass1.type = "text";
+    } else {
+        pass1.type = "password";
+    }
 });
+>>>>>>> parent of 010ea3f (Add files via upload)
